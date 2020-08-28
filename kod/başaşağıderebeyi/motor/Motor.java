f806940875e5d92fe145f1743053d9aedc5560ea
Cem GEÇGEL <aa-albara@hotmail.com>
2020-08-28 23:56:30 +0300

    Motor Sınıfı
    Motorun kalbi yazıldı. Şimdi onun çevresini arayüzlerle doldurmak var.

---
+++ kod/başaşağıderebeyi/motor/Motor.java
@@ -0,0 +1,94 @@
+/**
+ * başaşağıderebeyi.motor.Motor.java
+ * 0.1 / 28 Ağu 2020 / 23:22:45
+ * Cem GEÇGEL (BaşAşağıDerebeyi)
+ */
+package başaşağıderebeyi.motor;
+
+/** Motorun kalbi */
+public class Motor {
+	/** Sürüm */
+	public static final String SÜRÜM = "0.1";
+	/** Hedeflenen saniye başına kare oranı.
+	 * Eğer bu değer sıfır ise motor elinden geldiğince hızlı çalışır. */
+	public static float hedefKareOranı;
+	/** Motorun çalıştığını gösteren bayrak */
+	public static boolean çalışmakta;
+	/** Anlık saniye başına kare sayısı */
+	public static float kareOranı;
+	/** İşlenmeyi bekleyen kare sayısı */
+	public static float işlenmemişKareler;
+
+	/** Şuanki zamanı nano saniye cinsinden verir */
+	public static float zaman() {
+		return (float)System.nanoTime();
+	}
+
+	/** Motoru başlatır */
+	public static void başla() {
+		// Eğer motor zaten çalışıyorsa
+		if (çalışmakta)
+			// Motoru tekrar başlatma
+			return;
+		// Ana döngüyü çalıştır
+		çalışmakta = true;
+		çalış();
+	}
+	
+	/** Motoru durdur */
+	public static void dur() {
+/*		// Eğer motor zaten çalışmıyorsa
+		if (!çalışmakta)
+			// Motoru tekrar durdurma
+			return;
+		Bu kontrol gereksiz. */
+		çalışmakta = false;
+	}
+	
+	/** Ana döngüyü çalıştırır */
+	public static void çalış() {
+		try {
+			// Yükle
+			
+			// Ana döngüyü başlat
+			float öncekiZaman = zaman();
+			float saniyeSayacı = 0.0F;
+			int kareler = 0;
+			while (çalışmakta) {
+				// Geçen zamanı hesapla
+				float geçenZaman = zaman() - öncekiZaman;
+				öncekiZaman += geçenZaman;
+				// Bu kısımdan sonra geçen zaman saniye cinsinden
+				geçenZaman /= 1000000000.0F;
+				if (hedefKareOranı == 0.0F) {
+					kare();
+					kareler++;
+					işlenmemişKareler = 0.0F;
+				} else {
+					for (işlenmemişKareler += geçenZaman * hedefKareOranı;
+							işlenmemişKareler >= 1.0F;işlenmemişKareler--, kareler++)
+						kare();
+				}
+				// Her saniyede bir
+				if ((saniyeSayacı += geçenZaman) >= 1.0F) {
+					// Kare oranını hesapla
+					kareOranı = kareler / saniyeSayacı;
+					// Sıfırla
+					kareler = 0;
+					saniyeSayacı--;
+				}
+				// Dizüstümün yükünü bitirmemek için
+				Thread.sleep(5);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.exit(0);
+		}
+	}
+	
+	/** Kareyi işler */
+	private static void kare() {
+		
+	}
+}
